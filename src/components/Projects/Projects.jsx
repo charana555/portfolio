5b30d20587c5aed9e062d692b82e7e414c3cb370
charana555 <charanchandrashekar55@gamil.com>
2022-10-25 17:03:54 +0530

    Added project section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Image from "next/image";
+import { projects } from "../../data/projectdata";
+import { BiCodeAlt } from "react-icons/bi";
+import { BiLinkExternal } from "react-icons/bi";
+import styles from "../../styles/projects.module.css";
 
 const Projects = () => {
   return (
@@ -7,7 +12,41 @@ const Projects = () => {
         Projects
       </h2>
       <hr className=" mb-5" />
-      <p>This section is under progress</p>
+      <div className={styles.work_list}>
+        {projects.map((item) => {
+          return (
+            <div className={styles.work}>
+              <Image
+                className={styles.work_img}
+                src={item.image}
+                alt="work"
+                height={"400"}
+                width={"370"}
+              />
+              <div className={styles.layer}>
+                <h3 className={styles.layer_head}>{item.title}</h3>
+                <p className={styles.layer_para}>{item.desc}</p>
+                <div className="w-full flex justify-evenly items-center bg-transparent">
+                  <a
+                    target="_blank"
+                    className="text-black p-4 mt-8 bg-black rounded-[50%]"
+                    href={item.sourceLink}
+                  >
+                    <BiCodeAlt className=" bg-transparent text-2xl " />
+                  </a>
+                  <a
+                    target="_blank"
+                    className="text-black p-4 mt-8 bg-black rounded-[50%]"
+                    href={item.webLink}
+                  >
+                    <BiLinkExternal className=" bg-transparent text-2xl" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
---
+++ src/data/projectdata.js
@@ -0,0 +1,27 @@
+import music from "../images/music.jpg";
+import vote from "../images/vote.jpg";
+import work from "../images/work.jpg";
+
+export const projects = [
+  {
+    image: work,
+    title: "My WorkAt",
+    desc: "My WorkAt is a full stack freelencing website for people who need workers daily chores.",
+    sourceLink: "https://github.com/charana555/Myworat-next",
+    webLink: "",
+  },
+  {
+    image: vote,
+    title: "Online Voting",
+    desc: "Online Voting is a full stack website that helps people to conduct elections online.",
+    sourceLink: "https://github.com/charana555/react---voting---system",
+    webLink: "",
+  },
+  {
+    image: music,
+    title: "Music Player",
+    desc: "Music Player is online web based music listening platform.",
+    sourceLink: "",
+    webLink: "",
+  },
+];
